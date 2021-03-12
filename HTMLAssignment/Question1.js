67463fe45a0f254e9eef4ae5cb97f051783f21b7
Beimnet Tesfaye <chuni.yeab1@gmail.com>
2021-03-11 21:23:39 -0600

    week3day4Homework4

---
+++ HTMLAssignment/Question1.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<h1>
+    <title>
+        My Life History In Short
+    </title>
+
+</h1>
+
+<body>
+    <p>
+       
+    My name is<em> Beimnet</em> Tesfaye. I was born in Ethiopia in small village called Debre birhan. Debre birhan is a small 
+    town located 130 kilometers from Addis Ababa, the capital city of Ethiopia, it is a high land with a cold climate. The Average climate is 55oF.
+I came from a small family; I have an older sister who still live in Ethiopia. My mom she used to be a <strong>teacher</strong> and my dad used to be 
+a finance officer but they both are retire 2 years ago and now they are enjoying their retirement life.
+I can say I was lucky and blessed to be from small town. Even though, we don’t have too many options to choose from however, we had a lot of things 
+to do. We used to make our own toys, we go to the nearby river and swim, we used to go to a nearby forest and play with animals, we used to sing a song 
+and dance together and so on.For further reference please go to this link <href="https://google">
+    </p>
+    <p>
+        <a href="https://google">Validate</a>
+        </p>
+
+</body>
+</html>
---
+++ HTMLAssignment/question3.js
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>;
+<html>
+    <html>
+    <head>
+        <head>
+            <script>
+                let value1=null;
+                let value2=null;
+                let output=null;
+                function common(params) {
+                    value1=document.getElementById("val1");
+                    value2=document.getElementById("val2");
+                    output=document.getElementById("result");
+
+                    function add(params) {
+                        common();
+                    }
+                }
